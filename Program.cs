43b293405991879937645fd71f168ad27383bcf0
juli <chernetskayay.inbox.ru>
2022-12-16 11:56:24 +0300

    Comment

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum; 
+
+            Console.WriteLine("Сумма вклада:");
+            sum = Convert.ToDouble(Console.ReadLine());
+
+            if (sum < 100)
+            {
+                Console.WriteLine("много");
+            }
+        }
+    }
+}
